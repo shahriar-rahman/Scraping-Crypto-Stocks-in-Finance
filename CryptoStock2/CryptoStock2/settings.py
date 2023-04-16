41cf3d7c3bb23ff6538dd11ef9d9714e1ecbc0aa
Shahriar Rahman <shahriar.rahman10@northsouth.edu>
2023-04-16 19:54:39 +0600

    Settings File Updated
    • Introduced around 2200 user agent rotations to bypass crawl restriction.

--- CryptoStock2/CryptoStock2/settings.py
+++ CryptoStock2/CryptoStock2/settings.py
@@ -47,6 +47,10 @@ ROBOTSTXT_OBEY = True
 #SPIDER_MIDDLEWARES = {
 #    "CryptoStock2.middlewares.Cryptostock2SpiderMiddleware": 543,
 #}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
